41b35bc262ab0393008f5fa7e9dbb649ce5546b2
Sudbcotta <lucasnovaescola@yahoo.com.br>
2023-04-02 11:08:02 -0300

    https://github.com/Sudbcotta/TestePratico.git

---
+++ Javascript/main.js
@@ -0,0 +1,16 @@
+$('input').on('change', function() {
+    $('body').toggleClass('blue');
+  });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } 
+
+
+
+  
+ 
